15e13ce64f5a6492eef83a30f8ce9363abd2ee95
Jonathan May <jonathan.may@devmountain.com>
2019-01-27 18:48:34 -0600

    Update instructions

--- node-example/src/index.ts
+++ node-example/src/index.ts
@@ -5,7 +5,13 @@ import { users } from "./users";
  * Create a new array called greetings that is the result
  * of mapping over users and returning a string with
  * the user's first name and the number of
- * unread messages they have.
+ * unread messages they have. If they have 0 unread messages,
+ * only return a string saying hi and their first name.
  *
- * Ex. ["Hi Jonathan! You have 3 unread messages."]
+ * Ex. ["Hi Jonathan! You have 3 unread messages.",
+ *      "Hi James! You have 837 unread messages.",
+ *      "Hi Tim!",
+ *      "Hi Logan! You have 1 unread message.",
+ *      "Hi Garrett! You have 834672 unread messages."]
+ *
  */
